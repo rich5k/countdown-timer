9b5dc5e40b3f1d62e4ae3cc078f98e3c6192ff9b
rich5k <richardanatsui@gmail.com>
2021-10-26 02:53:10 +0100

    added javascript to index page by typescript

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const timer = document.getElementById("timer");
+timer.innerHTML = "00:40:00";
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+const timer = document.getElementById("timer") as HTMLElement;
+timer.innerHTML="00:40:00";
