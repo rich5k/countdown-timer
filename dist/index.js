a426003a62adda964586bb33b826646b39e90fe9
rich5k <richardanatsui@gmail.com>
2021-10-26 03:34:41 +0100

    added timer logic

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,31 @@
 "use strict";
 const timer = document.getElementById("timer");
+const time = timer.outerHTML;
+const timeBits = time.split(":");
+var hours = parseInt(timeBits[0]);
+var mins = parseInt(timeBits[1]);
+var secs = parseInt(timeBits[2]);
 const startbtn = document.getElementById("start");
 startbtn.addEventListener("click", () => {
-    timer.innerHTML = "00:40:00";
+    while (hours !== 0 || mins !== 0 || secs !== 0) {
+        if (secs !== 0) {
+            secs--;
+            timer.innerHTML = hours + ":" + mins + ":" + secs;
+        }
+        else {
+            if (mins !== 0) {
+                mins--;
+                secs = 59;
+                timer.innerHTML = hours + ":" + mins + ":" + secs;
+            }
+            else {
+                if (hours !== 0) {
+                    hours--;
+                    mins = 59;
+                    timer.innerHTML = hours + ":" + mins + ":" + secs;
+                }
+            }
+        }
+    }
+    timer.innerHTML = "00:00:00";
 });
