8e3fd5ef6b7d547fa2197a14c4c34bbf783c1197
rich5k <richardanatsui@gmail.com>
2021-10-29 03:57:17 +0100

    done setting up start timer functionality

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,17 @@
 "use strict";
 const timer = document.getElementById("timer");
-const time = timer.outerHTML;
+const time = timer.innerHTML;
 const timeBits = time.split(":");
 var setHours = parseInt(timeBits[0]);
 var setMins = parseInt(timeBits[1]);
 var setSecs = parseInt(timeBits[2]);
+console.log(setHours);
 const startbtn = document.getElementById("start");
 function getCountdownTime() {
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
     var currentDay = currentDate.getDate();
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
     var currentMonth = months[currentDate.getMonth()];
     var currentSeconds = currentDate.getSeconds();
     var addedSecs = currentSeconds + setSecs;
@@ -73,24 +74,31 @@ function getCountdownTime() {
             currentHours = addedHours.toString();
         }
     }
+    currentDay = currentDay + extraDay;
+    var countDownDate = new Date(`${currentMonth} ${currentDay}, ${currentYear} ${currentHours}:${currentMinutes}:${currentSeconds}`).getTime();
+    // console.log(setHours);
+    // timer.innerHTML=countDownDate.toString();
     // Update the count down every 1 second
-    // var x = setInterval(function() {
-    //     // Get todays date and time
-    //     var now = new Date().getTime();
-    //     // Find the distance between now an the count down date
-    //     var distance = countDownDate - now;
-    //     // Time calculations for days, hours, minutes and seconds
-    //     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    //     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    //     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    //     // Output the result in an element with id="timer"
-    //     timer.innerHTML=hours+":"+minutes+":"+seconds;
-    //     // If the count down is over, write some text 
-    //     if (distance < 0) {
-    //         clearInterval(x);
-    //         timer.innerHTML = "EXPIRED";
-    //     }
-    // }, 1000);
+    var x = setInterval(function () {
+        // Get todays date and time
+        var now = new Date().getTime();
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+        // Time calculations for days, hours, minutes and seconds
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Output the result in an element with id="timer"
+        var nhours = hours < 10 ? "0" + hours : hours;
+        var nminutes = minutes < 10 ? "0" + minutes : minutes;
+        var nseconds = seconds < 10 ? "0" + seconds : seconds;
+        timer.innerHTML = nhours + ":" + nminutes + ":" + nseconds;
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            timer.innerHTML = "EXPIRED";
+        }
+    }, 1000);
     // timer.innerHTML=currentHours+":"+currentMinutes+":"+currentSeconds;
 }
 startbtn.addEventListener("click", () => {
