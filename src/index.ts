0daeb0967cb3d34aa9cee7c061f0529c5a43725a
rich5k <richardanatsui@gmail.com>
2021-11-10 01:11:29 +0000

    added resumeTimer and timeoutCallback functions to TheTimer class

--- src/index.ts
+++ src/index.ts
@@ -146,17 +146,52 @@ class TheTimer{
     }
     pauseTimer():void{
         if(this.state!=1) return;
-        this.remaining =new Date().getTime()- this.startTime;
-        var hours = Math.floor((this.remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((this.remaining % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((this.remaining % (1000 * 60)) / 1000);
-        var nhours= hours <10 ? "0"+hours : hours;
-        var nminutes= minutes <10 ? "0"+minutes : minutes;
-        var nseconds = seconds <10 ? "0"+seconds : seconds;
+        this.remaining =1000-(new Date().getTime()- this.startTime);
+        // var hours = Math.floor((this.remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        // var minutes = Math.floor((this.remaining % (1000 * 60 * 60)) / (1000 * 60));
+        // var seconds = Math.floor((this.remaining % (1000 * 60)) / 1000);
+        // var nhours= hours <10 ? "0"+hours : hours;
+        // var nminutes= minutes <10 ? "0"+minutes : minutes;
+        // var nseconds = seconds <10 ? "0"+seconds : seconds;
         window.clearInterval(this.timerId);
         this.state=2;
-        console.log(this.remaining);
-        console.log("Remaining time: "+nhours+":"+nminutes+":"+nseconds);
+        // console.log(this.remaining);
+        // console.log("Remaining time: "+nhours+":"+nminutes+":"+nseconds);
+    }
+    timeoutCallback():void{
+        if(this.state!=3) return;
+        var countDownDate= this.getCountdownTime();
+        // this.startTime=new Date().getTime();
+        this.timerId= setInterval(()=>{
+            // Get todays date and time
+            var now = new Date().getTime();
+            this.startTime=now;
+            // Find the distance between now an the count down date
+            var distance = countDownDate - now;
+            
+            // Time calculations for days, hours, minutes and seconds
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+            var nhours= hours <10 ? "0"+hours : hours;
+            var nminutes= minutes <10 ? "0"+minutes : minutes;
+            var nseconds = seconds <10 ? "0"+seconds : seconds;
+            timer.innerHTML=nhours+":"+nminutes+":"+nseconds;
+                        
+            // If the count down is over, write some text 
+            if (distance < 0 && !isRepeat) {
+                clearInterval(this.timerId);
+                timer.innerHTML = "EXPIRED";
+            }
+        }, 1000);
+        this.state=1;
+    }
+    resumeTimer():void{
+        if(this.state!=2) return;
+
+        this.state=3;
+        setTimeout(this.timeoutCallback, this.remaining)
     }
 }
 
