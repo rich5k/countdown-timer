eebf847c273800c6610c27b4ae8b50e79c6d907f
rich5k <richardanatsui@gmail.com>
2021-11-16 00:21:59 +0000

    setup right toggle system for tiles

--- dist/pomodoro.js
+++ dist/pomodoro.js
@@ -5,13 +5,17 @@ var sess2 = document.getElementById("sess-2");
 var sess4 = document.getElementById("sess-4");
 pomo25.addEventListener('click', () => {
     pomo25.classList.toggle('selected-tile');
+    pomo50.classList.remove('selected-tile');
 });
 pomo50.addEventListener('click', () => {
     pomo50.classList.toggle('selected-tile');
+    pomo25.classList.remove('selected-tile');
 });
 sess2.addEventListener('click', () => {
     sess2.classList.toggle('selected-tile');
+    sess4.classList.remove('selected-tile');
 });
 sess4.addEventListener('click', () => {
     sess4.classList.toggle('selected-tile');
+    sess2.classList.remove('selected-tile');
 });
--- src/pomodoro.ts
+++ src/pomodoro.ts
@@ -5,16 +5,20 @@ var sess4= document.getElementById("sess-4") as HTMLElement;
 
 pomo25.addEventListener('click',()=>{
     pomo25.classList.toggle('selected-tile');
+    pomo50.classList.remove('selected-tile');
 })
 
 pomo50.addEventListener('click',()=>{
     pomo50.classList.toggle('selected-tile');
+    pomo25.classList.remove('selected-tile');
 })
 
 sess2.addEventListener('click',()=>{
     sess2.classList.toggle('selected-tile');
+    sess4.classList.remove('selected-tile');
 })
 
 sess4.addEventListener('click',()=>{
     sess4.classList.toggle('selected-tile');
+    sess2.classList.remove('selected-tile');
 })
