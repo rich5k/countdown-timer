b03bb2961ce1af4dcb502d6ee00de85b9463e763
rich5k <richardanatsui@gmail.com>
2022-01-21 16:44:20 +0000

    have to fix classList problem

--- dist/pomodoro-animate.js
+++ dist/pomodoro-animate.js
@@ -1,5 +1,8 @@
 var done = document.getElementById("done");
+var pomodoroSetting= document.getElementsByClassName("pomodoro-setting");
+var pomodoroTimer= document.getElementById("pomodoro-timer");
 done.addEventListener('click', () => {
     gsap.to(".pomodoro-setting", {duration:2, opacity:0, x:-300});
-    
+    pomodoroSetting.classList.add("hide-timer");
+    pomodoroTimer.classList.remove("hide-timer");
 });
