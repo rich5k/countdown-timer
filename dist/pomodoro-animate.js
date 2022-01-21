06fb3b14603772975d02c023dc492745c80dc087
rich5k <richardanatsui@gmail.com>
2022-01-21 01:43:45 +0000

    improved animation of tiles

--- dist/pomodoro-animate.js
+++ dist/pomodoro-animate.js
@@ -1,1 +1,5 @@
-gsap.to(".tiles", {duration:2, x:300});
+var done = document.getElementById("done");
+done.addEventListener('click', () => {
+    gsap.to(".tiles", {duration:2, opacity:0, x:-300});
+    
+});
