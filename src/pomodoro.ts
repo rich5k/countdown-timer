cccc4117de235e4152a913589739b1989eef240f
rich5k <richardanatsui@gmail.com>
2022-01-23 01:36:52 +0000

    added localstorage to pomodoro page

--- src/pomodoro.ts
+++ src/pomodoro.ts
@@ -6,20 +6,24 @@ console.log(pomo25);
 pomo25.addEventListener('click',()=>{
     pomo25.classList.toggle('selected-tile');
     pomo50.classList.remove('selected-tile');
+    localStorage.setItem("pomo","25");
 })
 
 pomo50.addEventListener('click',()=>{
     pomo50.classList.toggle('selected-tile');
     pomo25.classList.remove('selected-tile');
+    localStorage.setItem("pomo","50");
 })
 
 sess2.addEventListener('click',()=>{
     sess2.classList.toggle('selected-tile');
     sess4.classList.remove('selected-tile');
+    localStorage.setItem("sess","2");
 })
 
 sess4.addEventListener('click',()=>{
     sess4.classList.toggle('selected-tile');
     sess2.classList.remove('selected-tile');
+    localStorage.setItem("sess","4");
 })
 
