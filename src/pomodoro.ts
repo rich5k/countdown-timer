2fbdcaa2975d9d8eb17ca55b3789ca7b33b3b1bc
rich5k <richardanatsui@gmail.com>
2022-01-25 05:52:17 +0000

    have to fix localStorage comparing problem

--- src/pomodoro.ts
+++ src/pomodoro.ts
@@ -4,8 +4,7 @@ var sess2= document.getElementById("sess-2") as HTMLElement;
 var sess4= document.getElementById("sess-4") as HTMLElement;
 var sessions= document.getElementById("sessions") as HTMLElement;
 var timer = document.getElementById("timer")as HTMLElement;
-localStorage.removeItem('pomo');
-localStorage.removeItem('sess');
+localStorage.clear();
 
 pomo25.addEventListener('click',()=>{
     pomo25.classList.toggle('selected-tile');
@@ -30,15 +29,16 @@ sess4.addEventListener('click',()=>{
     sess2.classList.remove('selected-tile');
     localStorage.setItem("sess","4");
 })
-
-if(localStorage.getItem("sess")=="2"){
+var sessValue =+localStorage["sess"];
+var pomoValue =+localStorage["pomo"];
+if(sessValue==2){
     sessions.innerHTML="<li>P1: <em>not yet</em></li><li>P2: <em>not yet</em></li>"
 }
 else{
     sessions.innerHTML="<li>P1: <em>not yet</em></li><li>P2: <em>not yet</em></li><li>P3: <em>not yet</em></li><li>P4: <em>not yet</em></li>"
 }
 
-if(localStorage.getItem("pomo")=="50"){
+if(pomoValue==50){
     timer.innerHTML="00:50:00";
 }
 else{
