222f63c4b5684fd1d2682220c5d04b2a1655802d
rich5k <richardanatsui@gmail.com>
2022-03-11 10:49:45 +0000

    modified the timer sound settings

--- dist/timerSound.js
+++ dist/timerSound.js
@@ -1,14 +1,13 @@
 let tickingClock;
 let timeUpBeeper;
-let startBtn= document.getElementById('start');
 function preload(){
     soundFormats('mp3');
     tickingClock= loadSound('../assets/Clock-Ticking-C-www.fesliyanstudios.com.mp3');
+    timeUpBeeper= loadSound('../assets/Alarm-Fast-A1-www.fesliyanstudios.com.mp3');
 }
-function setup(){
-    tickingClock.play();
-}
-startBtn.addEventListener('click', ()=>{
-    
+// function setup(){
+//     tickingClock.play();
+// }
 
-})
+exports.tickingClock=tickingClock;
+exports.timeUpBeeper=timeUpBeeper;
